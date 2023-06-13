160a6429324744318480578c2d8b2512379ffda7
Веселов Владислав Русланович <veselov_v@tass.ru>
2023-06-13 21:18:13 +0300

    Implement Unpack function

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,51 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func toDigit(r rune) int {
+	return int(r - '0')
+}
+
+func isEmpty(r rune) bool {
+	return r == 0
+}
+
+func Unpack(str string) (string, error) {
+	builder := strings.Builder{}
+	var curLetter rune
+
+	for _, char := range str {
+		if unicode.IsDigit(char) {
+			// check, if digit goes after letter
+			if isEmpty(curLetter) {
+				return "", ErrInvalidString
+			}
+
+			// write to builder
+			digit := toDigit(char)
+			for i := 0; i < digit; i++ {
+				builder.WriteRune(curLetter)
+			}
+			curLetter = 0
+		} else {
+			// if letter without digit
+			if !isEmpty(curLetter) {
+				builder.WriteRune(curLetter)
+			}
+
+			curLetter = char
+		}
+	}
+
+	// write last letter to builder
+	if !isEmpty(curLetter) {
+		builder.WriteRune(curLetter)
+	}
+
+	return builder.String(), nil
 }
