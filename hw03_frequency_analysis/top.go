5f18f1de71807d59e82daa443a624a7bdabde964
Веселов Владислав Русланович <veselov_v@tass.ru>
2023-06-26 00:37:51 +0300

    Implement Top10 without asterisk task

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,56 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"sort"
+	"strings"
+)
+
+type wordCnt struct {
+	Word string
+	Cnt  int
+}
+
+func countFrequency(words []string) map[string]int {
+	freq := map[string]int{}
+	for _, word := range words {
+		if word != "" {
+			freq[word]++
+		}
+	}
+
+	return freq
+}
+
+func sortByFrequency(wordsFreq map[string]int) []string {
+	freq := []wordCnt{}
+	for word, cnt := range wordsFreq {
+		freq = append(freq, wordCnt{word, cnt})
+	}
+
+	sort.Slice(freq, func(i, j int) bool {
+		if freq[i].Cnt == freq[j].Cnt {
+			return freq[i].Word < freq[j].Word
+		}
+
+		return freq[i].Cnt > freq[j].Cnt
+	})
+
+	sortedWords := []string{}
+	for _, wc := range freq {
+		sortedWords = append(sortedWords, wc.Word)
+	}
+
+	return sortedWords
+}
+
+func Top10(s string) []string {
+	words := strings.Fields(s)
+	freq := countFrequency(words)
+	sorted := sortByFrequency(freq)
+
+	limit := 10
+	if len(sorted) < 10 {
+		limit = len(sorted)
+	}
+	return sorted[:limit]
 }
