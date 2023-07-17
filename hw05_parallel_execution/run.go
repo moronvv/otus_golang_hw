bd112df996dca77d7cd64031c238aac001d67cc0
Веселов Владислав Русланович <veselov_v@tass.ru>
2023-07-17 19:02:07 +0300

    Implement Run with race

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,6 +2,8 @@ package hw05parallelexecution
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 )
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
@@ -10,6 +12,74 @@ type Task func() error
 
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	// Place your code here.
-	return nil
+	taskCh := make(chan Task, len(tasks))
+
+	tasksCnt := 0
+	tasksMtx := sync.Mutex{}
+	allTasksDone := make(chan struct{})
+
+	errorsExceeded := make(chan struct{})
+	errMtx := sync.Mutex{}
+	errCnt := 0
+
+	quit := make(chan struct{})
+
+	for i := 0; i < n; i++ {
+		go func() {
+			for {
+				select {
+				case <-quit:
+					return
+				case task := <-taskCh:
+					if err := task(); err != nil {
+						func() {
+							errMtx.Lock()
+							defer errMtx.Unlock()
+
+							select {
+							case <-quit:
+								return
+							default:
+								errCnt++
+								if errCnt >= m {
+									close(quit)
+									close(errorsExceeded)
+								}
+							}
+						}()
+					}
+
+					func() {
+						tasksMtx.Lock()
+						defer tasksMtx.Unlock()
+
+						select {
+						case <-quit:
+							return
+						default:
+							tasksCnt++
+							if tasksCnt >= len(tasks) {
+								close(quit)
+								close(allTasksDone)
+							}
+						}
+					}()
+				}
+			}
+		}()
+	}
+
+	for _, task := range tasks {
+		taskCh <- task
+	}
+
+	<-quit
+	select {
+	case <-allTasksDone:
+		fmt.Println("success")
+		return nil
+	case <-errorsExceeded:
+		fmt.Println("fatal")
+		return ErrErrorsLimitExceeded
+	}
 }
