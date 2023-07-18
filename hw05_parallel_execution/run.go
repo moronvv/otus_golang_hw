35af320b7e9590b3f727edd55ccfdc0ee4c3c855
Веселов Владислав Русланович <veselov_v@tass.ru>
2023-07-18 12:53:16 +0300

    Working function without edge cases

--- hw05_parallel_execution/run.go
+++ hw05_parallel_execution/run.go
@@ -2,7 +2,6 @@ package hw05parallelexecution
 
 import (
 	"errors"
-	"fmt"
 	"sync"
 )
 
@@ -10,68 +9,47 @@ var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
 type Task func() error
 
-func worker(tasks chan Task, wg *sync.WaitGroup, allTasksDone chan struct{}, taskErr chan error, quit chan struct{}) {
-	defer wg.Done()
-
-	for {
-		select {
-		case <-quit:
-			return
-		case task, ok := <-tasks:
-			if !ok {
-				allTasksDone <- struct{}{}
-				return
-			}
-
-			if err := task(); err != nil {
-				taskErr <- err
-			}
-		}
-	}
-}
-
 // Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
 func Run(tasks []Task, n, m int) error {
-	tasksCh := make(chan Task, len(tasks))
-	var result error
-
-	taskErr := make(chan error)
-	allTasksDone := make(chan struct{})
-	quit := make(chan struct{})
-	var workersWg sync.WaitGroup
+	tasksCh := make(chan Task)
 
 	errCnt := 0
+	var errMtx sync.Mutex
+
+	var workersWg sync.WaitGroup
+	workersWg.Add(n)
 
 	for i := 0; i < n; i++ {
-		workersWg.Add(1)
-		go worker(tasksCh, &workersWg, allTasksDone, taskErr, quit)
+		// workers
+		go func() {
+			defer workersWg.Done()
+
+			for task := range tasksCh {
+				if err := task(); err != nil {
+					errMtx.Lock()
+					errCnt++
+					errMtx.Unlock()
+				}
+			}
+		}()
 	}
 
 	for _, task := range tasks {
+		errMtx.Lock()
+		errorsExceeded := errCnt >= m
+		errMtx.Unlock()
+		if errorsExceeded {
+			break
+		}
+
 		tasksCh <- task
 	}
+	close(tasksCh)
 
-loop:
-	for {
-		select {
-		case <-taskErr:
-			errCnt++
-			if errCnt >= m {
-				close(quit)
-				close(tasksCh)
-				fmt.Println("error")
-				result = ErrErrorsLimitExceeded
-				break loop
-			}
-		case <-allTasksDone:
-			close(tasksCh)
-			fmt.Println("done")
-			break loop
-		}
-	}
-
-	fmt.Println("wait all workers done")
 	workersWg.Wait()
 
-	return result
+	if errCnt >= m {
+		return ErrErrorsLimitExceeded
+	}
+	return nil
 }
