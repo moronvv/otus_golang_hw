67ac00798a4798706e8376e892214a97ae6a516e
Веселов Владислав Русланович <veselov_v@tass.ru>
2023-07-21 19:14:02 +0300

    Remove redundant slice for channels

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -9,23 +9,17 @@ type (
 type Stage func(in In) (out Out)
 
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	pipelineChans := []In{}
+	lastChan := in
 
-	// first chan - in + prepared slice of chans for every stage
-	pipelineChans = append(pipelineChans, in)
-	for i := 0; i < len(stages); i++ {
-		pipelineChans = append(pipelineChans, nil)
-	}
-
-	for i, stage := range stages {
+	for _, stage := range stages {
 		// out of prev stage == in of next stage
-		pipelineChans[i+1] = func(i int, stage Stage) Out {
+		lastChan = func(stage Stage, lastChan In) Out {
 			out := make(Bi)
 
 			go func() {
 				defer close(out)
 
-				for el := range stage(pipelineChans[i]) {
+				for el := range stage(lastChan) {
 					select {
 					case <-done:
 						return
@@ -35,9 +29,8 @@ func ExecutePipeline(in In, done In, stages ...Stage) Out {
 			}()
 
 			return out
-		}(i, stage)
+		}(stage, lastChan)
 	}
 
-	outCh := pipelineChans[len(stages)]
-	return outCh
+	return lastChan
 }
