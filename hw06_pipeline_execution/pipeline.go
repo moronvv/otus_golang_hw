44174e3517877b06673b24185a44f8a0129636c2
Веселов Владислав Русланович <veselov_v@tass.ru>
2023-07-21 16:31:24 +0300

    Implement pipeline with support of done signal

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -11,13 +11,31 @@ type Stage func(in In) (out Out)
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
 	pipelineChans := []In{}
 
+	// first chan - in + prepared slice of chans for every stage
 	pipelineChans = append(pipelineChans, in)
 	for i := 0; i < len(stages); i++ {
 		pipelineChans = append(pipelineChans, nil)
 	}
 
 	for i, stage := range stages {
-		pipelineChans[i+1] = stage(pipelineChans[i])
+		// out of prev stage == in of next stage
+		pipelineChans[i+1] = func(i int, stage Stage) Out {
+			out := make(Bi)
+
+			go func() {
+				defer close(out)
+
+				for el := range stage(pipelineChans[i]) {
+					select {
+					case <-done:
+						return
+					case out <- el:
+					}
+				}
+			}()
+
+			return out
+		}(i, stage)
 	}
 
 	outCh := pipelineChans[len(stages)]
