0f8afd13666c5701910712b8f210e914b231d252
Веселов Владислав Русланович <veselov_v@tass.ru>
2023-08-11 19:26:05 +0300

    Implement Copy function for existing test data + write tests

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"io"
+	"os"
+	"path/filepath"
 )
 
 var (
@@ -9,7 +12,82 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+// Get src file while checking edge cases.
+func getSrcFile(path string, offset int64, limit *int64) (*os.File, error) {
+	fileInfo, err := os.Stat(path)
+	if err != nil {
+		return nil, err
+	}
+	fileSize := fileInfo.Size()
+
+	// offset must be less then file size
+	if offset > fileSize {
+		return nil, ErrOffsetExceedsFileSize
+	}
+	// TODO: check inf file like /dev/urandom
+
+	// correct limit
+	if *limit == 0 {
+		*limit = fileSize
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	return file, nil
+}
+
+// Get dst file. Create dst path if not exists.
+func getDstFile(path string) (*os.File, error) {
+	file, err := os.Create(path)
+
+	// if dir not exists
+	if os.IsNotExist(err) {
+		// create path automatically
+		dir, _ := filepath.Split(path)
+		if err := os.MkdirAll(dir, os.ModeDir); err != nil {
+			return nil, err
+		}
+
+		// try to create file again
+		file, err = os.Create(path)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return file, err
+}
+
+// Copy from one file to another with offset/limit.
+func copyContent(fromFile, toFile *os.File, offset, limit int64) error {
+	if _, err := fromFile.Seek(offset, io.SeekStart); err != nil {
+		return err
+	}
+
+	if _, err := io.CopyN(toFile, fromFile, limit); err != nil {
+		if !errors.Is(err, io.EOF) {
+			return err
+		}
+	}
+
 	return nil
+}
+
+func Copy(fromPath, toPath string, offset, limit int64) error {
+	fromFile, err := getSrcFile(fromPath, offset, &limit)
+	if err != nil {
+		return err
+	}
+	defer fromFile.Close()
+
+	toFile, err := getDstFile(toPath)
+	if err != nil {
+		return err
+	}
+	defer toFile.Close()
+
+	return copyContent(fromFile, toFile, offset, limit)
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,92 @@
 package main
 
-import "testing"
+import (
+	"bytes"
+	"crypto/md5"
+	"io"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	const (
+		inPath = "./testdata/input.txt"
+	)
+
+	testCases := []struct {
+		name     string
+		testPath string
+		offset   int64
+		limit    int64
+	}{
+		{
+			name:     "input -> out_offset0_limit0",
+			testPath: "./testdata/out_offset0_limit0.txt",
+			offset:   0,
+			limit:    0,
+		},
+		{
+			name:     "input -> out_offset0_limit10",
+			testPath: "./testdata/out_offset0_limit10.txt",
+			offset:   0,
+			limit:    10,
+		},
+		{
+			name:     "input -> out_offset0_limit1000",
+			testPath: "./testdata/out_offset0_limit1000.txt",
+			offset:   0,
+			limit:    1000,
+		},
+		{
+			name:     "input -> out_offset0_limit10000",
+			testPath: "./testdata/out_offset0_limit10000.txt",
+			offset:   0,
+			limit:    10000,
+		},
+		{
+			name:     "input -> out_offset100_limit1000",
+			testPath: "./testdata/out_offset100_limit1000.txt",
+			offset:   100,
+			limit:    1000,
+		},
+		{
+			name:     "input -> out_offset6000_limit1000",
+			testPath: "./testdata/out_offset6000_limit1000.txt",
+			offset:   6000,
+			limit:    1000,
+		},
+	}
+
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(t *testing.T) {
+			// create temp file
+			outFile, err := os.CreateTemp("", "output.txt")
+			require.NoError(t, err)
+			outPath := outFile.Name()
+			defer os.Remove(outPath)
+
+			// run copy
+			err = Copy(inPath, outPath, tc.offset, tc.limit)
+			require.NoError(t, err)
+
+			// get dst file md5
+			outFileHash := md5.New()
+			_, err = io.Copy(outFileHash, outFile)
+			require.NoError(t, err)
+
+			// get test file md5
+			testFile, err := os.Open(tc.testPath)
+			require.NoError(t, err)
+			defer testFile.Close()
+			testFileHash := md5.New()
+			_, err = io.Copy(testFileHash, testFile)
+			require.NoError(t, err)
+
+			// compare md5 of test and dst files
+			require.True(t, bytes.Equal(testFileHash.Sum(nil), outFileHash.Sum(nil)))
+		})
+	}
 }
