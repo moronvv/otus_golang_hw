4c2cf8f466bfb6339626a94aca85611ec4567160
Веселов Владислав Русланович <veselov_v@tass.ru>
2023-08-20 14:42:26 +0300

    Implement RunCmd function

--- hw08_envdir_tool/executor.go
+++ hw08_envdir_tool/executor.go
@@ -1,7 +1,55 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
+)
+
+// Get all environment variables (system envs without unsetted + user envs).
+func getSystemAndUserEnvs(envs Environment) []string {
+	envsToRemove := map[string]string{}
+	for envName, envVal := range envs {
+		if envVal.NeedRemove {
+			envsToRemove[envName] = envVal.Value
+		}
+	}
+
+	finalEnvs := []string{}
+	for _, env := range os.Environ() {
+		split := strings.Split(env, "=")
+		envName := split[0]
+
+		if _, exists := envsToRemove[envName]; !exists {
+			finalEnvs = append(finalEnvs, env)
+		}
+	}
+
+	for envName, envVal := range envs {
+		if !envVal.NeedRemove {
+			finalEnvs = append(finalEnvs, fmt.Sprintf("%s=%s", envName, envVal.Value))
+		}
+	}
+
+	return finalEnvs
+}
+
 // RunCmd runs a command + arguments (cmd) with environment variables from env.
-func RunCmd(cmd []string, env Environment) (returnCode int) {
-	// Place your code here.
-	return
+func RunCmd(cmd []string, envs Environment) (returnCode int) {
+	if len(cmd) < 1 {
+		return 1
+	}
+	command := exec.Command(cmd[0], cmd[1:]...) //nolint:gosec
+
+	command.Env = getSystemAndUserEnvs(envs)
+
+	command.Stdin = os.Stdin
+	command.Stdout = os.Stdout
+
+	if err := command.Run(); err != nil {
+		return 1
+	}
+
+	return 0
 }
--- hw08_envdir_tool/main.go
+++ hw08_envdir_tool/main.go
@@ -1,5 +1,21 @@
 package main
 
+import (
+	"log"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	if len(os.Args) < 3 {
+		log.Fatal("too few arguments")
+	}
+	dirPath := os.Args[1]
+	cmd := os.Args[2:]
+
+	envs, err := ReadDir(dirPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	os.Exit(RunCmd(cmd, envs))
 }
